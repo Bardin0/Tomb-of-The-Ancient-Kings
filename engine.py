8b72018397e866f34d9a52f34398ae805be92f4f
Michael Giovannini <154377009+Bardin0@users.noreply.github.com>
2024-07-01 13:39:14 -0400

    Modified files to process entity generation within game_map

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
@@ -10,8 +10,7 @@ from game_map import GameMap
 from input_handlers import EventHandler
 
 class Engine: 
-    def __init__ (self, entities: set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__ (self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
@@ -39,11 +38,6 @@ class Engine:
         self.game_map.explored |= self.game_map.visible
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-
-        for entity in self.entities:
-            # Only print entites in FOV
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
