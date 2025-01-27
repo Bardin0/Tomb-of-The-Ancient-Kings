7f949beaaf7885eb58ab501c6aeed0233c865c44
Michael Giovannini <154377009+Bardin0@users.noreply.github.com>
2025-01-26 19:25:55 -0500

    Added more to frontend.py

--- frontend.py
+++ frontend.py
@@ -13,7 +13,7 @@ st.markdown('''
     - various monsters and items found within the world.
     - Infinite Levels! (Obviously)
             
-    To try out the game simply hit the run button below
+    To try out the game simply hit the run button below (It will open a new window).
 
 ''')
 
@@ -23,6 +23,61 @@ if st.button("Run Game"):
 st.markdown('''
 
     Creator: Michael Giovannini
+            
     [Source Code](https://github.com/Bardin0/rougue-lite)
+
+''')
+
+st.header("How to Play", divider="rainbow")
+st.markdown('''
+
+    Objective:
+    There is no real objective, just see how far you can go!
+            
+    Controls:
+            
+    Up: j or up arrow
+
+    Down: k or down arrow
+
+    Left: h or left arrow
+
+    Right l or right arrow
+
+    Diagonal up and left: y or home
+
+    Diagonal up and right: u or page up
+
+    Diagonal down and left: b or end 
+
+    Diagonal down and right: n or page down
+
+            
+    Inventory: i
+
+    Character Menu: c
+
+    Pickup: g
+
+    Drop Menu: d
+
+    Close Menu / Select: Enter
+
+    Tile selector: Forward slash (/)
+
+    Descend Floors: >
+
+    View Message History: v
+
+            
+            
+    Quit: Escape
+            
+    How do you attack you might ask?
+
+    Just walk into the enemies!
+
+    Have Fun!
+
 
 ''')
